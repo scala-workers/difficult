72e8fa1a607ec904d7d8432b8ae4bfcdd58c34f9
漓江丽火 <909134790@qq.com>
2023-09-12 13:41:25 +0800

    Update depts.

--- src/main/scala/test01/Test02.scala
+++ src/main/scala/test01/Test02.scala
@@ -1,12 +1,15 @@
 package test01
 
 import cats.*
+import cats.syntax.*
+import cats.implicits._
 import cats.effect.*
 import com.github.kwhat.jnativehook.GlobalScreen
 import com.github.kwhat.jnativehook.NativeHookException
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener
 import fs2.*
+import org.apache.pekko.Done
 import org.apache.pekko.actor.typed.ActorSystem
 import sample.killrweather.fog.WeatherStation
 
@@ -48,10 +51,8 @@ class GlobalKeyListenerExample(val instance: ActorSystem[WeatherStation.Command]
 
 object Test0211111111111 extends IOApp.Simple {
 
-  override val run: IO[Unit] = {
-    val instance                           = CatchKeybordImpl.gen
-    val actorSystem                        = ActorSystem.create(WeatherStation(instance), "uusdrlsdfsdnkwe")
-    val listener: GlobalKeyListenerExample = new GlobalKeyListenerExample(actorSystem)
+  class ExecImpl(actorSys: ActorSystem[WeatherStation.Command], instance: CatchKeybordImpl) {
+    val listener: GlobalKeyListenerExample = new GlobalKeyListenerExample(actorSys)
 
     val stream: Stream[IO, Char] = Stream.unfoldEval(instance) { ins =>
       for (r <- ins.toIO) yield Some(r -> ins.tail)
@@ -59,19 +60,40 @@ object Test0211111111111 extends IOApp.Simple {
 
     val action = stream.mapAsync(10)(t => IO(println(t)))
 
-    IO.delay {
-      try {
-        GlobalScreen.registerNativeHook()
-      } catch {
-        case ex: NativeHookException =>
-          System.err.println("There was a problem registering the native hook.")
-          System.err.println(ex.getMessage())
+    val execAction = IO
+      .delay {
+        try {
+          GlobalScreen.registerNativeHook()
+        } catch {
+          case ex: NativeHookException =>
+            System.err.println("There was a problem registering the native hook.")
+            System.err.println(ex.getMessage())
 
-          System.exit(1)
+            System.exit(1)
+        }
+
+        GlobalScreen.addNativeKeyListener(listener)
       }
-
-      GlobalScreen.addNativeKeyListener(listener)
-    }.flatTap(_ => action.compile.drain)
+      .flatTap(_ => action.compile.drain)
   }
 
+  override val run: IO[Unit] = {
+    val instance = CatchKeybordImpl.gen
+
+    def actorSystemInstanceGen = ActorSystem.create(WeatherStation(instance), "uusdrlsdfsdnkwe")
+    val actorSystemResource: Resource[IO, ActorSystem[WeatherStation.Command]] = ActorSystemResources(IO(actorSystemInstanceGen)).resource
+
+    actorSystemResource.use(actorSys => ExecImpl(actorSys, instance).execAction)
+  }
+
+}
+
+class ActorSystemResources[F[_], T](val actorSystem: F[ActorSystem[T]]) {
+  private def closeAction[UF[_]: Async, U](actorSys: ActorSystem[U]): UF[Done] = for
+    unitDone   <- Sync[UF].delay(actorSys.terminate())
+    done: Done <- Async[UF].fromFuture(Sync[UF].delay(actorSys.whenTerminated))
+  yield done
+
+  def resource(using Async[F]): Resource[F, ActorSystem[T]] =
+    Resource.make(actorSystem)(sys => for done: Done <- closeAction(sys) yield done)
 }
