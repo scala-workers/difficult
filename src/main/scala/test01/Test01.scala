e7a7a3afa2968d576bc423d1df41ffc451b7ca20
漓江丽火 <909134790@qq.com>
2023-09-15 11:12:39 +0800

    Update depts.

--- src/main/scala/test01/Test01.scala
+++ src/main/scala/test01/Test01.scala
@@ -1,11 +1,9 @@
-package bb
-package cc
+package bb.cc
 
-import java.awt.Robot
 import cats.*
 import cats.implicits.given
 import cats.effect.*
-import com.caoccao.javet.annotations.V8Function
+import com.caoccao.javet.annotations.{V8Function, V8Property}
 import com.caoccao.javet.interop.NodeRuntime
 import com.caoccao.javet.utils.JavetOSUtils
 import com.caoccao.javet.interop.engine.{IJavetEngine, IJavetEnginePool, JavetEnginePool}
@@ -17,7 +15,10 @@ import org.apache.pekko.Done
 import java.io.File
 import scala.concurrent.Promise
 
-class HaveATest(catchFunc: Done => Unit) {
+class HaveATest(catchFunc: Done => Unit, volumeValue: Int) {
+  @V8Property
+  def getVolumeValue: Int = volumeValue
+
   @V8Function(name = "finished")
   def finished: Unit = {
     println("喵呜呜完成啦lalalalala")
@@ -25,71 +26,23 @@ class HaveATest(catchFunc: Done => Unit) {
   }
 }
 
-object Test01 extends IOApp.Simple {
+object Test01 extends IOApp.Simple:
 
-  def exec[F[_]: Async](nodeRuntime: NodeRuntime): F[Done] = {
-    val execString: String =
-      """
-        |const loudness = require('loudness')
-        |loudness.setVolume(13)
-        |testValue.finished()
-        |""".stripMargin
-    def execAction: Unit = {
-      nodeRuntime.getExecutor(execString).executeVoid()
-      nodeRuntime.await()
-    }
-
-    val promise = Promise[Done]
-    val future  = promise.future
-    val finishedIO: F[Done] = Async[F].fromFuture(Sync[F].delay(future)).map { t =>
-      println("miaowuwu" * 100)
-      t
-    }
-
-    def objResource(v8Obj: V8ValueObject): Resource[F, HaveATest] = Resource.make(Sync[F].delay(().match
-      case _ =>
-        val model = HaveATest(done => promise.trySuccess(done))
-        v8Obj.bind(model)
-        model
-    ))(bindedObj => Sync[F].delay(v8Obj.unbind(bindedObj)))
-
-    def resourceAction: Resource[F, (V8ValueGlobalObject, V8ValueObject)] = for
-      v8ValueObject  <- Resource.fromAutoCloseable(Sync[F].delay(nodeRuntime.createV8ValueObject()))
-      globalObject   <- Resource.fromAutoCloseable(Sync[F].delay(nodeRuntime.getGlobalObject))
-      haveATestModel <- objResource(v8ValueObject)
-    yield globalObject -> v8ValueObject
-
-    resourceAction.use((_, _).match
-      case (globalObj, v8ValueObj) =>
-        val action: F[Done] = Sync[F].delay(().match
-          case _ =>
-            globalObj.set("testValue", v8ValueObj)
-            execAction
-            Done
-        )
-
-        for
-          done1: Done <- action
-          done2: Done <- finishedIO
-        yield done2
-    )
-
-  }
-
-  def execResource(r: Resource[IO, NodeRuntime]): IO[Done] = r.use(runtime => exec[IO](runtime))
-
-  val runDone: IO[Done] = {
+  def runDone[F[_]: Async]: F[Done] =
     val runtimeResource = for
-      given IJavetEnginePool[NodeRuntime] <- V21AAA.resource[IO]
-      given IJavetEngine[NodeRuntime]     <- V1aa(summon).resource[IO]
-    yield ToNodeRuntime(summon).resource[IO]
+      given IJavetEnginePool[NodeRuntime] <- V21AAA.resource[F]
+      given IJavetEngine[NodeRuntime]     <- V1aa(summon).resource[F]
+      given Resource[F, NodeRuntime] = ToNodeRuntime(summon).resource[F]
+    yield ExecAction(summon)
 
-    runtimeResource.use(v => execResource(v))
-  }
+    runtimeResource.use(v => v.setVolume)
+  end runDone
 
-  override val run: IO[Unit] = runDone.map(identity)
+  def toUNIt[F[_]: Functor](f: F[Done]): F[Unit] = for (done: Done <- f) yield done
 
-}
+  override val run: IO[Unit] = toUNIt(runDone[IO])
+
+end Test01
 
 class V1aa(bb: IJavetEnginePool[NodeRuntime]):
   def resource[F[_]: Sync]: Resource[F, IJavetEngine[NodeRuntime]] = Resource.fromAutoCloseable(Sync[F].delay(bb.getEngine))
@@ -131,3 +84,63 @@ class ToNodeRuntime(pool: IJavetEngine[NodeRuntime]):
   yield runtimeImpl
 
 end ToNodeRuntime
+
+class ExecAction[F[_]](nodeRuntimeResource: Resource[F, NodeRuntime]):
+  def setVolume(using Async[F]): F[Done] = nodeRuntimeResource.use(runtime => ExecActionImpl(runtime).action)
+end ExecAction
+
+class ExecActionImpl(nodeRuntime: NodeRuntime):
+
+  def action[F[_]: Async]: F[Done] =
+    val execString: String =
+      """
+        |const loudness = require('loudness')
+        |async function setV(numValue) {
+        |  await loudness.setVolume(numValue)
+        |  testValue.finished()
+        |}
+        |setV(testValue.volumeValue)
+        |""".stripMargin
+
+    def execAction: Unit =
+      nodeRuntime.getExecutor(execString).executeVoid()
+      nodeRuntime.await()
+    end execAction
+
+    val promise             = Promise[Done]
+    val future              = promise.future
+    val finishedIO: F[Done] = Async[F].fromFuture(Sync[F].delay(future))
+
+    def objResource(v8Obj: V8ValueObject): Resource[F, HaveATest] = Resource.make(
+      Sync[F].delay(
+        ().match
+          case _ =>
+            val model = HaveATest(done => promise.trySuccess(done), volumeValue = 20)
+            v8Obj.bind(model)
+            model
+      )
+    )(bindedObj => Sync[F].delay(v8Obj.unbind(bindedObj)))
+
+    def resourceAction: Resource[F, (V8ValueGlobalObject, V8ValueObject)] = for
+      v8ValueObject  <- Resource.fromAutoCloseable(Sync[F].delay(nodeRuntime.createV8ValueObject()))
+      globalObject   <- Resource.fromAutoCloseable(Sync[F].delay(nodeRuntime.getGlobalObject))
+      haveATestModel <- objResource(v8ValueObject)
+    yield globalObject -> v8ValueObject
+
+    resourceAction.use((_, _).match
+      case (globalObj, v8ValueObj) =>
+        val action: F[Done] = Sync[F].delay(().match
+          case _ =>
+            globalObj.set("testValue", v8ValueObj)
+            execAction
+            Done
+        )
+
+        for
+          done1: Done <- action
+          done2: Done <- finishedIO
+        yield done2
+    )
+  end action
+
+end ExecActionImpl
